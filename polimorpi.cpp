eed9baa12e5655eed02cf17b7e1d4e35507253cb
sykllh <sykllhidmi@gmail.com>
2026-05-18 14:43:09 +0700

    membuat class joko

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -4,4 +4,9 @@ using namespace std;
 class seseorang {
 public:
     virtual void pesan() = 0;
-}
+    // virtual void pesan(){
+    //      cout<<"Pesan dari seseorang"<<endl;
+    // }
+};
+
+class joko :
