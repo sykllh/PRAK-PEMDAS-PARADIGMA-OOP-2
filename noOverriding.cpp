fd22a37b092308fce9f6223cfff7e4bf37133c9a
sykllh <sykllhidmi@gmail.com>
2026-05-18 14:54:42 +0700

    membuat class derivedClass

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 class baseClass {
 public:
+    virtual void perkenalan() final{
+        cout << "Halo saya function dari base class";
+    }
+};
+class derivedClass : public baseClass {
     
 }
