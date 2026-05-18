b0f2e903912769486cb96df40813588f7c154c91
sykllh <sykllhidmi@gmail.com>
2026-05-18 14:55:50 +0700

    membuat program utamanya

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -8,5 +8,11 @@ public:
     }
 };
 class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Halo saya function dari derived Class";
+    }
+};
+int main() {
     
 }
